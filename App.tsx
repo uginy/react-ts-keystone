90b117adfd89f52a958e8d99d88fbf3a24dfe409
uginy <42149942+uginy@users.noreply.github.com>
2022-12-11 16:40:37 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -3,49 +3,60 @@ import * as React from 'react';
 import './style.css';
 import { useRootStore } from './store';
 import { Checkbox, Input } from 'antd';
-import { getSnapshot } from 'mobx-keystone';
-import { useForm, Controller } from 'react-hook-form';
-import { UiStore } from './store/ui-store';
+import { applySnapshot, getSnapshot } from 'mobx-keystone';
+import { useForm, Controller, useFormState, FormProvider } from 'react-hook-form';
 
-export const App = observer(() => {
+export const App = () => {
   const { uiStore } = useRootStore();
 
-  const onChange = (value: UiStore) => {
-    uiStore.setCheck1(value.Check1);
-    uiStore.setCheck2(value.Check2);
-    uiStore.setInput(value.Input);
-  };
+  const methods  =
+    useForm({
+      defaultValues: getSnapshot(uiStore),
+    });
 
-  const { control, handleSubmit, getValues } = useForm({
-    defaultValues: getSnapshot(uiStore)
+const { control, handleSubmit, getValues, setValue, watch } = methods;
+ 
+  const { dirtyFields, touchedFields } = useFormState({
+    control,
   });
 
- console.log(getSnapshot(uiStore))
+  React.useEffect(()=> {
+    watch((data)=>{
+    applySnapshot(uiStore, data);
+    console.log( 1, getSnapshot(uiStore))
+  })
+  },[])
+
   return (
-    <form onClick={handleSubmit(onChange)}>
-      <Controller
-        name="Check1"
-        control={control}
-        render={({ field }) => (
-          <Checkbox {...field} checked={uiStore.Check1}>
-            Checkbox
-          </Checkbox>
-        )}
-      />
-       <Controller
-        name="Check2"
-        control={control}
-        render={({ field }) => (
-          <Checkbox {...field} checked={uiStore.Check2}>
-            Checkbox
-          </Checkbox>
-        )}
-      />
-      <Controller
-        name="Input"
-        control={control}
-        render={({ field }) => <Input {...field}/>}
-      />
-    </form>
+    <FormProvider {...methods}>
+      <form>
+        <Controller
+          name="Check1"
+          control={control}
+          render={({ field }) => (
+            <Checkbox {...field} checked={field.value}>
+              Checkbox1
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="Check2"
+          control={control}
+          render={({ field }) => (
+            <input
+              type="checkbox"
+              {...field}
+              checked={field.value}
+              onChange={(e) => field.onChange(e.target.checked)}
+            />
+          )}
+        />
+        <Controller
+          name="Input"
+          control={control}
+          render={({ field }) => <Input {...field} />}
+        />
+      </form>
+    </FormProvider>
   );
-});
+};
